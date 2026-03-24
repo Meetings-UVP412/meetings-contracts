5a22180d02f1e7b5647c700f5c23524e83cb4cdf
Michael <michaelkivo977@gmail.com>
2026-03-24 09:10:47 +0300

    add /getAudioChunk

--- src/main/java/demo/meetingscontracts/endpoints/MeetingsApi.java
+++ src/main/java/demo/meetingscontracts/endpoints/MeetingsApi.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
@@ -45,4 +46,10 @@ public interface MeetingsApi {
                       @RequestParam("isLast") Boolean isLast,
                       @RequestParam("m-uid") UUID uuid
     );
+
+    @Tag(name = "Meeting", description = "Встречи")
+    @Operation(summary = "Получение аудио файла")
+    @ApiResponse(responseCode = "200", description = "Файл получен!")
+    @GetMapping("/{uuid}/chunks/{ord}")
+    ResponseEntity<byte[]> getAudioChunk(@PathVariable UUID uuid, @PathVariable Integer ord);
 }
